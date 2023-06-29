1776c003b2737f1b6749f1c659939131102b543f
Anthhon <anthcomercial@gmail.com>
2023-06-29 14:48:28 -0300

    feature: getpos and movement functions implemented

--- lib/static_ui.h
+++ lib/static_ui.h
@@ -2,7 +2,7 @@
 #define STATIC_UI_H
 
 // Draws a border using the specified character
-void termui_draw_border(char border_char){
+void termui_draw_border(const char border_char){
 	termui_get_windows_size(); // Updates window size
 	size_t x = (size_t)term_info.x_win_size;
 	size_t y = (size_t)term_info.y_win_size;
@@ -20,7 +20,7 @@ void termui_draw_border(char border_char){
 }
 
 // Creates a box with the specified dimensions and border character
-void termui_box_create(const size_t x, const size_t y, char border_char){
+void termui_box_create(const size_t x, const size_t y, const char border_char){
 	for (size_t i = 1; i <= x; ++i){
 		for (size_t j = 1; j <= y; ++j){
 			if (j != 1 && j != y && i != 1 && i != x){
@@ -33,7 +33,7 @@ void termui_box_create(const size_t x, const size_t y, char border_char){
 }
 
 // Creates a box with the specified dimensions, border character, and starting coordinates
-void termui_box_create_at(const size_t x, const size_t y, const size_t x_coord, const size_t y_coord, char border_char){
+void termui_box_create_at(const size_t x, const size_t y, const size_t x_coord, const size_t y_coord, const char border_char){
 	// Build box
 	for (size_t i = 1; i <= x; ++i){
 		for (size_t j = 1; j <= y; ++j){
@@ -47,38 +47,28 @@ void termui_box_create_at(const size_t x, const size_t y, const size_t x_coord, 
 }
 
 // Creates a text box with a message inside and a border
-void termui_text_box(char *message, char border_char, const size_t offset){
-	// Build box
+// TODO: Text box cannot deal with '\n' characters
+void termui_text_box(const char *message, const size_t offset, const char border_char){
 	const size_t x = strlen(message) + offset * 2 + 2;
 	const size_t y = offset * 2 + 3;
-	for (size_t i = 1; i <= x; ++i){
-		for (size_t j = 1; j <= y; ++j){
-			if (j != 1 && j != y && i != 1 && i != x){
-				continue;
-			}
-			termui_move_to(i, j);
-			printf("%c", border_char);
-		}
-	}
+
+	// Build box
+	termui_box_create(x, y, border_char);
+
 	// Print message
 	termui_move_to(offset * 2, offset * 2);
 	printf("%s", message);
 }
 
 // Creates a text box with a message inside, a border, and starting coordinates
-void termui_text_box_at(char *message, char border_char, const size_t offset, const size_t x_coord, const size_t y_coord){
-	// Build box
+// TODO: Text box cannot deal with '\n' characters
+void termui_text_box_at(const char *message, const size_t offset, const size_t x_coord, const size_t y_coord, const char border_char){
 	const size_t x = strlen(message) + offset * 2 + 2;
 	const size_t y = offset * 2 + 3;
-	for (size_t i = 1; i <= x; ++i){
-		for (size_t j = 1; j <= y; ++j){
-			if (j != 1 && j != y && i != 1 && i != x){
-				continue;
-			}
-			termui_move_to(i + x_coord, j + y_coord);
-			printf("%c", border_char);
-		}
-	}
+
+	// Build box
+	termui_box_create_at(x, y, x_coord, y_coord, border_char);
+
 	// Print message at given coordinate
 	termui_move_to(offset * 2 + x_coord, offset * 2 + y_coord);
 	printf("%s", message);
--- lib/termui.h
+++ lib/termui.h
@@ -7,26 +7,48 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
+#define CURSOR_X_COORD_ADDR &term_info.x_cursor_coord
+#define CURSOR_Y_COORD_ADDR &term_info.y_cursor_coord
+
 typedef struct {
 	size_t x_win_size; // Horizontal window size
 	size_t y_win_size; // Vertical window size
+	size_t x_cursor_coord; // X cursor coordinates
+	size_t y_cursor_coord; // Y cursor coordinates
 } TerminalStatus;
 TerminalStatus term_info;
 
-// Retrieves the cursor position from the terminal
-// TODO: Remove escape char echo in terminal when executing
-void termui_get_cursor_pos()
-{
-	int x, y;
+void termui_move_to(const size_t x, const size_t y);
 
-	printf("\033[6n");
+// Stores the cursor position
+void termui_get_pos(void){
+	int x, y;
+	int t = STDOUT_FILENO;
+	struct termios old_terminal_settings;
+
+	// Get the current terminal settings
+	tcgetattr(t, &old_terminal_settings);
+
+	// Disable echoing and canonical mode
+	struct termios new_terminal_settings = old_terminal_settings;
+	new_terminal_settings.c_lflag &= ~(ECHO | ICANON);
+
+	// Apply the modified terminal settings
+	tcsetattr(t, TCSANOW, &new_terminal_settings);
+
+	// Request cursor position
+	printf("\033[6n"); 
 	fflush(stdout);
-	scanf("\033[%d;%dR", &y, &x);
-	printf("Cursor Position: x = %d, y = %d\n", x, y);
+
+	// Store cursor position
+	scanf("\033[%d;%dR", CURSOR_Y_COORD_ADDR, CURSOR_X_COORD_ADDR);
+
+	// Restore the original terminal settings
+	tcsetattr(t, TCSANOW, &old_terminal_settings);
 }
 
 // Retrieves the window size of the terminal
-void termui_get_windows_size(){
+void termui_get_windows_size(void){
 	struct winsize size;
 
 	ioctl(0, TIOCGWINSZ, &size); // Uses ioctl system call to update terminal size values
@@ -34,6 +56,29 @@ void termui_get_windows_size(){
 	term_info.y_win_size = size.ws_row;
 }
 
+void termui_go_left(void){
+	// Stores actual position
+	termui_get_pos();
+	termui_move_to(term_info.x_cursor_coord - 1, term_info.y_cursor_coord);
+}
+
+void termui_go_right(void){
+	// Stores actual position
+	termui_get_pos();
+	termui_move_to(term_info.x_cursor_coord + 1, term_info.y_cursor_coord);
+}
+
+void termui_go_up(void){
+	// Stores actual position
+	termui_get_pos();
+	termui_move_to(term_info.x_cursor_coord, term_info.y_cursor_coord - 1);
+}
+
+void termui_go_down(void){
+	// Stores actual position
+	termui_get_pos();
+	termui_move_to(term_info.x_cursor_coord, term_info.y_cursor_coord + 1);
+}
 
 // Moves the cursor to the specified coordinates
 void termui_move_to(const size_t x, const size_t y){
